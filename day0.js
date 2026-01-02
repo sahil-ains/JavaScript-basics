d86b55f3c548f23dd866878d295eba3751413a6e
sahil-ains <252408145+sahil-ains@users.noreply.github.com>
2026-01-02 23:05:16 +0530

    Day 2: conditions and control flow

---
+++ day0.js
@@ -0,0 +1,6 @@
+let age = 16;
+if (age >= 18){
+    console.log("you are adult");
+}else{
+    console.log("miner");
+}
---
+++ day2.js
@@ -0,0 +1,69 @@
+let num =0;
+if (num > 0 ){
+    console.log ("positive");
+}else if (num < 0) {
+    console.log("negative");
+}else {
+    console.log("zero");
+}
+let num2 = -5;
+if (num2 > 0){
+    console.log ("positive");
+}else if (num2 < 0){
+    console.log("negative");
+}else {
+    console.log("zero");
+}
+let num3 = 3;
+if (num3 > 0 ){
+    console.log ("positive");
+}else if (num3 < 0){
+    console.log("negative");
+}else{
+    console.log("zero")
+}
+let num4=10;
+if (num4 % 2===0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
+let num5=9;
+if (num5 %2===0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
+let age=17;
+if (age >=18){
+    console.log("Eligible");
+}else{
+    console.log("Not eligible");
+}
+let age2=19;
+let age3=20;
+if (age2 >=18 && age3 >=18){
+    console.log("Eligible");
+}else{
+    console.log("Not Eligible");
+}
+let x=10;
+let y=20;
+if (x>y) {
+    console.log(x);
+}else{
+    console.log(y);
+}
+let a=68;
+let b=69;
+if (a>b){
+    console.log(a);
+}else{
+    console.log(b);
+}
+let age4 = 16;
+if (age >= 18){
+    console.log("you are adult");
+}else{
+    console.log("miner");
+}
