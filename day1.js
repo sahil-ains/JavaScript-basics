0184fa49153e54fee2c2c63004ae616a38626913
sahil-ains <252408145+sahil-ains@users.noreply.github.com>
2026-01-02 16:57:31 +0530

    Day 1: JavaScript basics

---
+++ day1.js
@@ -0,0 +1,37 @@
+console.log("Day 1 started");
+console.log("line 1");
+console.log(2 + 3);
+let age = 30;
+let name = "YourName";
+console.log(age + 5);
+console.log(`My name is ${name}`);
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(`sum is ${a + b}`);
+console.log(`Next year my age will be ${age + 1}`);
+let year =2026;
+console.log(`Next year will be be ${year+1}`);
+console.log(`My age after 10 years will be ${age+10}`);
+let x=10;
+let y=2;
+console.log(x+y);
+console.log(x*y);
+console.log(x/y);
+console.log(`Addition: ${x+y}`);
+console.log(`multipication: ${x*y}`);
+console.log(`Division: ${x/y}`);
+console.log(`x%y=${x%y}`);
+let num = 7;
+if (num % 2 === 0) {
+    console.log('even');
+} else {
+    console.log('odd');
+}
+console.log (a+b);
+let length=10;
+let width=5;
+console.log (length*width);
+let celsius = 30
+let fahrenheit=(celsius*9/5)+32
+console.log(fahrenheit);
