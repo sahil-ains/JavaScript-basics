0f169c8776cef5440c2719f6d9be08c3d3be9896
sahil-ains <252408145+sahil-ains@users.noreply.github.com>
2026-01-03 18:53:37 +0530

    Day 3: fixed accumulator variable bug

---
+++ day3.js
@@ -0,0 +1,34 @@
+for (let i = 1; i <= 5 ; i++) {
+    console.log(i);
+}
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+for (let i = 1; i <=20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+for (i = 1; i <= 30; i++) {
+    if (i % 2 === 0 ){
+        console.log(i);
+    }
+}
+for (i = 10; i >= 1; i--) {
+    console.log(i);
+}
+let sum = 0;
+for (let i = 1; i <=5; i++) {
+    sum = sum + i;
+}
+console.log("sum is", sum);
+let sum1 = 0;
+for (let i = 1; i <=10; i++) {
+    sum1 = sum1 + i;
+}
+console.log("sum is", sum1);
+let sum2 = 0;
+for (let i = 3; i <= 7; i++) {
+    sum2 = sum2 + i;
+}
+console.log("sum is", sum2);
